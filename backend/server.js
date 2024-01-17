9601a90f1788279c3391545625b410a95ef184fd
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-01-17 14:51:36 -0800

    error and exception handling

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+
+// Send raw json
+app.use(express.json());
+// Urlencoded form
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome Help Desk API' });
@@ -11,5 +17,6 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
