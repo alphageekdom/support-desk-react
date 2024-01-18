b8ea2fe007fb5324448cf8ae71834e13a5c98319
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-01-18 15:27:16 -0800

    Home, Loging, & Register UI

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,22 @@
+import { Link } from 'react-router-dom';
+import { FaQuestionCircle, FaTicketAlt } from 'react-icons/fa';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <section className='heading'>
+        <h1>What do you need help with?</h1>
+        <p>Please choose an option below:</p>
+      </section>
+
+      <Link to={'/new-ticket'} className='btn btn-reverse btn-bock'>
+        <FaQuestionCircle /> Create New Ticket
+      </Link>
+      <Link to={'/tickets'} className='btn btn-bock'>
+        <FaTicketAlt /> View My Tickets
+      </Link>
+    </>
+  );
 };
 
 export default Home;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,68 @@
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import { FaSignInAlt } from 'react-icons/fa';
+
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>
+          <FaSignInAlt />
+        </h1>
+        <p>Please login to get support</p>
+      </section>
+
+      <div className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
+              value={email}
+              onChange={onChange}
+              placeholder='Enter Email'
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              value={password}
+              onChange={onChange}
+              placeholder='Enter Password'
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <button className='btn btn-block'>Submit</button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Login;
